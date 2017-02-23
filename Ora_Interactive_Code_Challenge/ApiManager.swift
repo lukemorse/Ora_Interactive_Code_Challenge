088b661b4cc9ef102f000211f11e7c210a56b11b
lukemorse <luke.morse@gmail.com>
2017-02-22 21:01:52 -0500

    Wrote function to parse JSON and store auth token and user data

--- Ora_Interactive_Code_Challenge/ApiManager.swift
+++ Ora_Interactive_Code_Challenge/ApiManager.swift
@@ -13,14 +13,38 @@ class ApiManager: NSObject {
     
     static let sharedInstance = ApiManager()
     
+    var userEmail: String?
+    var userID: Int?
+    var userName: String?
+    var authToken: String?
+    
     let URL_BASE = "https://private-93240c-oracodechallenge.apiary-mock.com/"
     
     func getAuthToken() {
         
         Alamofire.request(URL_BASE + "auth/login", method: .post).responseJSON { response in
             
-            if let JSON = response.response {
-                print("JSON: \(JSON)")
+//            print("RESPONSE:"); print(response.response)
+//            print("DATA: "); print(response.data)
+//            print("RESULT: "); print(response.result)
+//            print("VALUE: "); print(response.value)
+            
+            if let JSON = response.response?.allHeaderFields {
+                let jsonDict = JSON as! [String: String]
+                self.authToken = jsonDict["Authorization"]
+            }
+            
+            if let jsonVal = response.value {
+                if let jsonDict = jsonVal as? [String: Any] {
+                    print(jsonDict)
+                    if let data = jsonDict["data"] as? [String:AnyObject] {
+                        print(data)
+                        
+                        self.userID = data["id"] as! Int?
+                        self.userEmail = data["email"] as! String?
+                        self.userName = data["name"] as! String?
+                    }
+                }
             }
             
             switch response.result {
